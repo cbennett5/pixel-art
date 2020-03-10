73b12815004ce9cd31bfa90d62d60c0c0b171b20
Christian Bennett <cbennett@stripe.com>
2020-03-09 21:19:58 -0400

    Adding clicking ability

---
+++ artMaker.js
@@ -0,0 +1,40 @@
+window.onload = function(){
+    let current_color = 'red';
+    
+    let colorChoices = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+    let myPalette = document.querySelector("#palette");
+
+    for(let color of colorChoices) {
+        
+        let box = document.createElement('div');
+        box.style.height = '15px';
+        box.style.width = '15px';
+        box.style.border = '1px solid black';
+        box.style.backgroundColor = color;
+        box.setAttribute("id","color_selector");
+        box.addEventListener('click',function(event) {
+            current_color = event.target.style.backgroundColor
+        })
+        myPalette.appendChild(box)
+    }
+    
+    let container = document.querySelector("#canvas")
+
+        for(let i =0; i < 400; i++) {
+            let box = document.createElement('span');
+            box.style.height = '8px';
+            box.style.width = '8px';
+            box.style.border = '1px solid black';
+            box.setAttribute("id",i);
+            box.addEventListener('click',function(event) {
+                event.target.style.backgroundColor = current_color
+            }) 
+
+            container.appendChild(box);
+
+        }
+        document.body.appendChild(container);
+        this.document.body.appendChild;
+let clickPixel = document.querySelector('span');
+
+}
